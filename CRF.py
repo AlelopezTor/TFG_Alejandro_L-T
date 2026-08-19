c3f63c7b5c0cd4a2b8263b0a2db4afdf2ed36a53
unknown <ale.tormos@gmail.com>
2026-08-19 21:58:09 +0200

    Feat: se hace la función train de la clase CFR y se ha empezado la función __cfr

--- CRF.py
+++ CRF.py
@@ -1,3 +1,4 @@
+import random
 class Node:
     infoset: str = ''
     n_actions: int
@@ -31,12 +32,84 @@ class Node:
         return avg_strategy
     
 class CFR:
-    CHEKC: int = 0 #pasar si nadie apuesta
-    BET: int = 1 #apostar
-    CALL: int = 2 #igualar la apuesta
-    RAISE: int = 3
-    FOLD: int = 4 #retirarse
+    CHEKC: str = 'c' #pasar si nadie apuesta
+    BET: str = 'b' #apostar
+    CALL: str = 'ca' #igualar la apuesta
+    RAISE: str = 'r'
+    FOLD: str = 'f' #retirarse
+    BETS: tuple[int, int] = (2, 4)
     node_map: dict[str, Node]
+    DECK = ['J', 'J', 'Q', 'Q', 'K', 'K']
+
+    def __solve_winner(self, cards):
+        if cards[0] == cards[2]:
+            return 0
+        elif cards[1] == cards[2]:
+            return 1
+        elif cards[0] == cards[1]:
+            return -1
+        else:
+            return 0 if cards[0] > cards[1] else 1
+            
+            
+            
+            
+    def __cfr(self, cards: list[str], history: str, p0: float, p1: float) -> float:
+        last_round = history.split('/')[-1]
+        to_move:int = len(last_round) % 2
+        loser: int = 1 - to_move
+        loser_count: int = 1
+        winner_count: int = 1
+        if history.endswith(self.FOLD):
+            for r, round in enumerate(history.split('/')):
+                for i, ch in enumerate(round):
+                    if ch == self.BET or ch == self.RAISE:
+                        if i%2 == to_move:
+                            winner_count = loser_count + self.BETS[r]
+                        else:
+                            loser_count = winner_count + self.BETS[r]
+                    if ch == self.CALL:
+                        if i%2 == to_move:
+                            winner_count = loser_count
+                        else:
+                            loser_count = winner_count
+            return loser_count
+        elif history.split('/')[-1].endswith(self.CHEKC+self.CHEKC) and len(history.split('/')) > 1:
+            winner = self.__solve_winner(cards)
+            if winner == -1:
+                return 0
+            for r, round in enumerate(history.split('/')):
+                for i, ch in enumerate(round):
+                    if ch == self.BET or ch == self.RAISE:
+                        if i%2 == to_move:
+                            winner_count = loser_count + self.BETS[r]
+                        else:
+                            loser_count = winner_count + self.BETS[r]
+                    if ch == self.CALL:
+                        if i%2 == to_move:
+                            winner_count = loser_count
+                        else:
+                            loser_count = winner_count
+            
+            return loser_count if winner == to_move else -loser_count
+            
+        
+                    
+    def train(self, iterations:int):
+        random.seed()
+        util: float = 0
+        for i in range (iterations):
+            random.shuffle(self.DECK)
+            cards = random.sample(self.DECK, 3)
+            util += self.__cfr(cards, '', 1, 1)
+        print(f"Media de valor por iteración: {util / iterations}")
+        for node in self.node_map.values():
+            print(node.infoset)
+        
+    
+                    
+        
+            
     
     
 
